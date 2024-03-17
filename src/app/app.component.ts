823bbb1490b3d7facf9edb3646b93a0518f55abd
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2024-03-17 15:18:58 +0100

    added menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NgbNav, NgbNavItem, NgbNavLink} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgbNavItem, NgbNavLink, NgbNav],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
